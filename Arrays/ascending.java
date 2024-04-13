43394aadfeb6efca97b6a0c376254cee9ca0c741
Sanjana-Ahlawat09 <149235944+Sanjana-Ahlawat09@users.noreply.github.com>
2024-04-13 07:17:58 +0000

    ascending

---
+++ Arrays/ascending.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ascending {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] numbers = new int[size];
+        for(int i=0;i<size;i++){
+            numbers[i] = sc.nextInt();
+        }
+        boolean isAscending = true;
+        for(int i=0;i<numbers.length-1;i++){
+            if(numbers[i]>numbers[i+1]){
+                isAscending = false;
+            }
+            if(isAscending){
+                System.out.println("THE ARRAY IS IN SORTED ASCENDING ORDER");
+            }
+            else{
+                System.out.println("THE ARRAY IS NOT SORTED IN ASCENDING ORDER");
+            }
+        }
+
+    }
+}
